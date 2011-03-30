3a0097ea6db25724bebbb9c4eb5de69e55644b22
Lu WangShen <tka@Lu-WangShenteki-MacBook-Pro.local>
2011-03-30 17:04:34 +0800

    make preference panel look better

--- src/ui/preference_panel.rb
+++ src/ui/preference_panel.rb
@@ -28,44 +28,46 @@ class PreferencePanel
  
     font_data=@shell.getFont().getFontData()
     font_data.each do |fd|
-      fd.setHeight(14)
+      fd.setHeight(12)
     end
     font=Swt::Graphics::Font.new(@display, font_data)
 
     button_group =Swt::Widgets::Composite.new(@shell, Swt::SWT::NO_MERGE_PAINTS );
     layout = Swt::Layout::RowLayout.new(Swt::SWT::VERTICAL) 
     layout.marginBottom = 0;
-    layout.spacing = 2;
+    layout.spacing = 10;
     button_group.setLayout( layout );
-    data = Swt::Layout::FormData.new(400,70)
-    button_group.setLayoutData(data)
 
     @button_v11 = Swt::Widgets::Button.new(button_group, Swt::SWT::RADIO )
     @button_v11.setText("v0.11")
     @button_v11.setSelection( App::CONFIG['use_version'] == 0.11 || !(App::CONFIG['use_specify_gem_path'] || App::CONFIG['use_version']) )
-    
+    @button_v11.setFont(font)
+
     @button_v10 = Swt::Widgets::Button.new(button_group, Swt::SWT::RADIO )
     @button_v10.setText("v0.10")
     @button_v10.setSelection( App::CONFIG['use_version'] == 0.10 )
+    @button_v10.setFont(font)
 
 
     button = Swt::Widgets::Button.new(button_group, Swt::SWT::RADIO )
     button.setText("Use specify gem path")
     button.setSelection(App::CONFIG['use_specify_gem_path'])
+    button.setFont(font)
     button.addListener(Swt::SWT::Selection,Swt::Widgets::Listener.impl do |method, evt|   
       @gem_path_text.setEnabled(evt.widget.getSelection)
-      @gem_path_choose_btn.setEnabled(evt.widget.getSelection)
       @speial_gem_label.setEnabled(evt.widget.getSelection)
     end)
     @use_specify_gem_path_btn=button
 
-    data = Swt::Layout::FormData.new(480, 35)
+    data = Swt::Layout::FormData.new(480, 50)
     data.left = Swt::Layout::FormAttachment.new( button_group, 22, Swt::SWT::LEFT)
     data.top = Swt::Layout::FormAttachment.new( button_group, 0, Swt::SWT::BOTTOM)
     @speial_gem_label = Swt::Widgets::Label.new( @shell, Swt::SWT::LEFT | Swt::SWT::WRAP)
-    @speial_gem_label.setText("Compass.app comes with some default extensions. if you want use RubyGem to manage extensions, you can specify your own gem path.")
+    @speial_gem_label.setText("Compass.app comes with some default extensions. if you want use RubyGem to manage extensions, you can specify your own gem path.\nex: /usr/local/lib/ruby/gems/1.8:/Users/foo/.gems")
     @speial_gem_label.setLayoutData(data)
     @speial_gem_label.setEnabled(@use_specify_gem_path_btn.getSelection)
+    @speial_gem_label.setFont(font)
+
 
     layout = Swt::Layout::FormLayout.new()
     layout.marginHeight=0
@@ -77,26 +79,12 @@ class PreferencePanel
     group.setLayout(layout)
     group.setLayoutData(data)
 
-    data = Swt::Layout::FormData.new(360, 20)
+    data = Swt::Layout::FormData.new(480, 20)
     text = Swt::Widgets::Text.new(group, Swt::SWT::BORDER)
     text.setText(App::CONFIG['gem_path'] || '')
     text.setEnabled(@use_specify_gem_path_btn.getSelection)
     text.setLayoutData(data)
     @gem_path_text=text
-
-    data = Swt::Layout::FormData.new()
-    data.left = Swt::Layout::FormAttachment.new(text, 5)
-    data.top = Swt::Layout::FormAttachment.new(text, 0, Swt::SWT::CENTER)
-    button = Swt::Widgets::Button.new(group, Swt::SWT::PUSH )
-    button.setText("Choose...")
-    button.setLayoutData(data)
-    button.setEnabled(@use_specify_gem_path_btn.getSelection)
-    button.addListener(Swt::SWT::Selection,Swt::Widgets::Listener.impl do |method, evt|   
-      dia = Swt::Widgets::DirectoryDialog.new(@shell)
-      dir = dia.open
-      @gem_path_text.setText(dir) if dir
-    end)
-    @gem_path_choose_btn=button
 
     data = Swt::Layout::FormData.new()
     data.left = Swt::Layout::FormAttachment.new(group, -20, Swt::SWT::LEFT)
