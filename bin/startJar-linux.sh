7b30ea00c66e4a44208d1df10637176da16a84f8
tka <tkatka@gmail.com>
2011-06-15 00:23:51 +0800

    add java param for gc info

--- bin/startJar-linux.sh
+++ bin/startJar-linux.sh
@@ -3,4 +3,6 @@ if [ "$1" = "full" ]; then
   rake rawr:clean
 fi
 rake rawr:jar
-java -client -Xverify:none  -Xbootclasspath/a:lib/java/jruby-complete.jar -jar package/jar/compass-app.jar
+java -verbose:gc -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -Djruby.reify.classes=true \
+  -Xms64m -Xmn32m \
+-client -Xverify:none  -Xbootclasspath/a:lib/java/jruby-complete.jar -jar package/jar/compass-app.jar
--- bin/startJar-osx.sh
+++ bin/startJar-osx.sh
@@ -3,4 +3,6 @@ if [ "$1" = "full" ]; then
   rake rawr:clean
 fi
 rake rawr:jar
-java -d32 -client -Xverify:none -XstartOnFirstThread -Xbootclasspath/a:lib/java/jruby-complete.jar -jar package/jar/compass-app.jar
+#java -verbose:gc -XX:+PrintGCTimeStamps -XX:+PrintGCDetails \
+#  -Xms128m  -Xmn32m -Xmx128m \
+java  -d32 -client -Xverify:none -XstartOnFirstThread -Xbootclasspath/a:lib/java/jruby-complete.jar -jar package/jar/compass-app.jar
