f09981b51901fe27501049dc72e9cc435685204d
tka <tkatka@gmail.com>
2011-06-20 21:05:55 +0800

    fix #23, remove installation (unzip) path limitation

--- src/main.rb
+++ src/main.rb
@@ -11,14 +11,6 @@ end
 
 require "swt_wrapper"
 
-if Dir.pwd =~ / /
-  display  = Swt::Widgets::Display.get_current 
-  msgbox=Swt::Widgets::MessageBox.new(Swt::Widgets::Shell.new(display), Swt::SWT::ICON_ERROR| Swt::SWT::OK)
-  msgbox.setMessage("Compass.app is running in \n\n#{Dir.pwd}\n\nThis path has space , please move Compass.app to another path")
-  msgbox.open
-  return 
-end
-
 
 require 'stringio'
 require 'thread'
