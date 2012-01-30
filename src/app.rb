756204058fd96af8ad486d2703f2f0fc4125860f
tka lu <tkatka@gmail.com>
2012-01-30 22:12:19 +0800

    version dump 1.12

--- src/app.rb
+++ src/app.rb
@@ -4,7 +4,7 @@ module App
   extend self
 
   include CompileVersion
-  VERSION = "1.11"
+  VERSION = "1.12"
   OS = org.jruby.platform.Platform::OS 
   OS_VERSION = java.lang.System.getProperty("os.version")
 
