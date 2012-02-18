b792674b6118d399843e4cfb6533e5921374b2ed
tka lu <tkatka@gmail.com>
2012-02-19 01:59:44 +0800

    add Haml support and better erb support just create #{project_path}/something.html.erb  or #{project_path}/something.html.haml and open http://127.0.0.1:24680

--- src/simplehttpserver.rb
+++ src/simplehttpserver.rb
@@ -1,19 +1,26 @@
 require "singleton"
 require "webrick";
+require "erb"
+require "hamlhandler.rb"
+
+WEBrick::HTTPServlet::FileHandler.add_handler("haml", WEBrick::HTTPServlet::HamlHandler)
 
 class SimpleHTTPServer
   include Singleton
   include WEBrick
-
   def start(dir, options)
-    
+
     options={
       :Port => 24680
     }.merge(options)
     stop
     @http_server = HTTPServer.new(options) unless @http_server
     @http_server_thread = Thread.new do 
-      @http_server.mount("/",HTTPServlet::FileHandler, dir, true);
+      @http_server.mount("/",HTTPServlet::FileHandler, dir,  {
+        :AcceptableLanguages => [:erb, :haml],
+        :FancyIndexing => true
+      });
+      
       @http_server.start
     end
   end
