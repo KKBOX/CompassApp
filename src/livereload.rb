1b3f84ca5a948c77d862ec8b99d66916c84cf94d
tka lu <tkatka@gmail.com>
2012-02-19 19:23:34 +0800

    #58 support using LiveReload without browser extensions

--- src/livereload.rb
+++ src/livereload.rb
@@ -1,6 +1,41 @@
 require "singleton"
 require 'em-websocket'
 require 'json'
+module EventMachine
+  module WebSocket
+    class Connection 
+      def receive_data(data)
+        debug [:receive_data, data]
+
+        if @handler
+          @handler.receive_data(data)
+        else
+          dispatch(data)
+        end 
+      rescue HandshakeError => e
+        debug [:error, e]
+        trigger_on_error(e)
+        # Errors during the handshake require the connection to be aborted
+
+        #abort # comment for failover normailhttp request
+
+      rescue WebSocketError => e
+        debug [:error, e]
+        trigger_on_error(e)
+        close_websocket_private(1002) # 1002 indicates a protocol error
+      rescue => e
+        debug [:error, e]
+        # These are application errors - raise unless onerror defined
+        trigger_on_error(e) || raise(e)
+        # There is no code defined for application errors, so use 3000
+        # (which is reserved for frameworks)
+        close_websocket_private(3000)
+      end 
+
+    end
+  end
+end
+
 class SimpleLivereload
   include Singleton
   attr_accessor :clients
@@ -39,9 +74,20 @@ class SimpleLivereload
         ws.onmessage do |msg|
           puts "Browser URL: #{msg}"
         end
+
         ws.onclose do
           SimpleLivereload.instance.clients.delete ws
           puts "Browser disconnected."
+        end
+
+        ws.onerror do |error|
+          # for http://help.livereload.com/kb/general-use/using-livereload-without-browser-extensions
+          if error.kind_of?(EventMachine::WebSocket::HandshakeError)
+            ws.send_data "HTTP/1.1 200 OK\r\n\r\n"+ open(File.join(LIB_PATH, 'javascripts', "livereload.js")).read
+            ws.close_connection_after_writing
+          else
+            ws.about
+          end
         end
       end
     end
@@ -71,7 +117,7 @@ class SimpleLivereload
     @watch_project_thread = Thread.new do
       FSSM.monitor do |monitor|
         monitor.path dir do |path|
-          path.glob '**/*.{css,png,jpg,gif,js,html}'
+          path.glob '**/*.{css,png,jpg,gif,js,html,erb,haml}'
           path.update do |base, relative|
             puts ">>> Change detected to: #{relative}"
             SimpleLivereload.instance.send_livereload_msg( base, relative )
