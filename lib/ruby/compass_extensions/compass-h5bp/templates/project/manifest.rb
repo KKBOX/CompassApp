c3d973fcf31be1188711b8f65a3089fcfd847616
tka lu <tkatka@gmail.com>
2012-03-18 22:53:30 +0800

    update compass-h5bp , fix #70

--- lib/ruby/compass_extensions/compass-h5bp/templates/project/manifest.rb
+++ lib/ruby/compass_extensions/compass-h5bp/templates/project/manifest.rb
@@ -1,4 +1,23 @@
 Compass.configuration.javascripts_dir= "js"
 Compass.configuration.css_dir= "css"
 Compass.configuration.images_dir= "img"
-discover :all
+
+discover :stylesheets
+
+dir = File.dirname(__FILE__)
+Dir.glob("#{dir}/**/*").each do |file|
+  next if /manifest\.rb/ =~ file
+  next if /.*\.scss/ =~ file
+  short_name = file[(dir.length+1)..-1]
+  options = {}
+  ext = if File.extname(short_name) == ".erb"
+          options[:erb] = true
+          File.extname(short_name[0..-5])
+        else
+          File.extname(short_name)
+        end[1..-1]
+        file_type = :file 
+        file_type = :directory if File.directory?(file)
+        send(file_type, short_name, options)
+end 
+
