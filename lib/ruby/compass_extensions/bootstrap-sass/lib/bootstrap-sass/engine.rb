259ebe04ecbdf224859d77e8fc2ab34eaf0beef6
tka.lu <tkatka@gmail.com>
2012-06-20 10:46:12 +0800

    update bootstrap-sass

--- lib/ruby/compass_extensions/bootstrap-sass/lib/bootstrap-sass/engine.rb
+++
@@ -1,9 +0,0 @@
-module Bootstrap
-  module Rails
-    class Engine < ::Rails::Engine
-      # Rails, will you please look in our vendor? kthx
-      # also add our initializer.
-      # paths["config/initializers"] << 'lib/bootstrap-sass/config'
-    end
-  end
-end
