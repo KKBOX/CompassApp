9989ceb91dbd64ab5583c6bf671fd3fa4f3ca80a
tka lu <tkatka@gmail.com>
2012-11-20 13:03:28 +0800

    compass.app without active icon

--- src/ui/tray.rb
+++ src/ui/tray.rb
@@ -11,11 +11,9 @@ class Tray
 
     if org.jruby.platform.Platform::IS_MAC
       @standby_icon = App.create_image("icon/16_dark@2x.png")
-      @active_icon = App.create_image("icon/16_white@2x.png")
       @watching_icon = App.create_image("icon/16@2x.png")
     else 
       @standby_icon = App.create_image("icon/16_dark.png")
-      @active_icon = App.create_image("icon/16_white.png")
       @watching_icon = App.create_image("icon/16.png")
     end
 
