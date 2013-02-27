17f2aced066a7e80fb63ffc0277ac2d3ca6e1eb2
tka.lu <tkatka@gmail.com>
2013-02-27 14:08:49 +0800

    bump version 1.22

--- src/app.rb
+++ src/app.rb
@@ -4,7 +4,7 @@ module App
   extend self
 
   include CompileVersion
-  VERSION = "1.21"
+  VERSION = "1.22"
   OS = org.jruby.platform.Platform::OS 
   OS_VERSION = java.lang.System.getProperty("os.version")
 
