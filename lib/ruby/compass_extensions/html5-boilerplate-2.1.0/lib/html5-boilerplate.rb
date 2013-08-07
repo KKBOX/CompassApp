7638bf39ef4bbcd2515671ffef9307d7635d2c44
HondaDai <hondadai.tw@gmail.com>
2013-08-07 15:43:43 +0800

    upgrade html5-boilerplate 2.0.0 -> 2.1.0

--- lib/ruby/compass_extensions/html5-boilerplate-2.1.0/lib/html5-boilerplate.rb
+++ lib/ruby/compass_extensions/html5-boilerplate-2.1.0/lib/html5-boilerplate.rb
@@ -1,4 +1,4 @@
-Compass::Frameworks.register("html5-boilerplate", :path => "#{File.dirname(__FILE__)}/..")
+Compass::Frameworks.register("html5-boilerplate", :path => "#{File.dirname(__FILE__)}/..") if defined?(Compass)
 
 if defined?(ActionController)
   require File.join(File.dirname(__FILE__), 'app', 'helpers', 'html5_boilerplate_helper')
