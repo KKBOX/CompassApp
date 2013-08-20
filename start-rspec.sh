a70f9c0bf499846962de01169185bb36ef628faa
tka.lu <tkatka@gmail.com>
2013-08-20 21:07:05 +0800

    fix start-rspec.sh in linux

--- start-rspec.sh
+++ start-rspec.sh
@@ -7,5 +7,10 @@ source $(rvm 1.6.8 do rvm env --path)
 # Note: make sure you are in **jruby**
 #jruby -J-XstartOnFirstThread rspec spec/create_project_swtbot.rb
 #jruby -J-XstartOnFirstThread rspec spec/main_spec.rb
-jruby -J-XstartOnFirstThread rspec $1
+os=$(uname)
+if [ $os == "Linux" ]; then
+  jruby  rspec $1
+else # for mac
+  jruby -J-XstartOnFirstThread rspec $1
+fi
 
