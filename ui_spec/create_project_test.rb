6168a71c8e4918a64080df62890f1971283f20a6
HondaDai <hondadai.tw@gmail.com>
2013-08-27 11:25:01 +0800

    can create project

---
+++ ui_spec/create_project_test.rb
@@ -0,0 +1,65 @@
+
+require File.join(File.dirname(__FILE__), 'ui_spec_helper.rb')
+
+describe "create_project_test" do
+  
+  bot = SwtBot.new(Tray.instance.shell, Tray.instance.menu)
+
+  describe "create by FileDialog path" do
+    it "should new a Compass Project" do
+      bot.menu('Create Compass Project').menu('compass').menu('project')
+    end
+  end
+
+  describe "create by DirectoryDialog path" do
+    it "should new a Compass Project" do
+      _path = Swt::Widgets::FileDialog.open_path
+      Swt::Widgets::FileDialog.open_path = Swt::Widgets::DirectoryDialog.open_path
+
+      puts _path
+      bot.menu('Create Compass Project').menu('compass').menu('project')
+      
+      Swt::Widgets::FileDialog.open_path = _path
+    end
+  end
+
+end 
+
+#bot.menu('Quit').click
+
+=begin
+bot = SwtBot.new(Tray.instance.shell, Tray.instance.menu)
+
+
+
+
+
+#bot.menu('Watch a Folder...').click
+#bot.menu('Create Compass Project').menu('blueprint').menu('buttons').click
+
+#puts Tray.instance.dialog
+
+
+
+Compass::Frameworks::ALL.each do | framework |
+  next if framework.name =~ /^_/
+  next if framework.template_directories.empty?
+  #puts framework.name
+  framework.template_directories.each do | dir |
+    #puts "  "+dir.to_s
+  end
+end
+
+
+#describe Main do
+  #when "create project ''" do
+    #it "" do
+    #end
+  #end
+#end
+=end
+
+
+#Main.run_tray
+
+
