984fd5f0d7f42eea4754c400a2f80f417593e506
HondaDai <hondadai.tw@gmail.com>
2013-08-28 02:06:43 +0800

    add build_test_data.sh

---
+++ ui_spec/test_data/build_test_data.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+ORIGIN_DIR=$(pwd)
+BASE_DIR=$(dirname $0)
+
+IMAGES_DIR="images"
+SRC_DIR="sass"
+SASS_FILE="$SRC_DIR/swt_test.scss"
+
+relative_assets=(enable disable)
+line_comments=(enable disable)
+debug_info=(enable disable)
+output_style=(compact compressed expanded nested)
+
+
+for r in ${relative_assets[@]}; do
+  for l in ${line_comments[@]}; do
+    for d in ${debug_info[@]}; do
+      for o in ${output_style[@]}; do
+        CSS_DIR="${r}_relative_assets/${l}_line_comments/${d}_debug_info/$o"
+        
+        options=""
+        if [ "$r" == "enable" ]; then
+          options="$options --relative-assets"
+        fi
+        if [ "$l" == "disable" ]; then
+          options="$options --no-line-comments"
+        fi
+        if [ "$d" == "enable" ]; then
+          options="$options --debug-info"
+        fi
+        options="$options --output-style $o "
+
+        echo $CSS_DIR
+        #mkdir -p "$CSS_DIR"
+        compass compile $BASE_DIR --css-dir $CSS_DIR --sass-dir $SRC_DIR $options --quiet
+      done
+    done
+  done
+done
+
