8a917a50f04e3420061fc6cc0d4179a3b5a8d2b7
HondaDai <hondadai.tw@gmail.com>
2013-09-02 15:28:57 +0800

    history

--- src/app.rb
+++ src/app.rb
@@ -146,7 +146,8 @@ module App
   end
 
   def set_histoy(dirs)
-    dirs = dirs[0, App::CONFIG["num_of_history"]]
+    dirs = dirs.uniq[0, App::CONFIG["num_of_history"]]
+    puts dirs
     File.open(HISTORY_FILE, 'w') do |out|
       YAML.dump(dirs, out)
     end 
