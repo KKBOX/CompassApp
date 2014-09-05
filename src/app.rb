b0a0d2c4644c2d3a1e1f666c559c8027b391ef7b
hlb <hlb@handlino.com>
2014-09-05 19:05:54 +0800

    Revert "bump v.1.28.beta.1"
    This reverts commit c3b9617e567371c84ddf0d209924adc1676f2297.

--- src/app.rb
+++ src/app.rb
@@ -5,7 +5,7 @@ module App
   extend self
 
   include CompileVersion
-  VERSION = "1.28.beta.1"
+  VERSION = "1.27"
   OS = org.jruby.platform.Platform::OS 
   OS_VERSION = java.lang.System.getProperty("os.version")
 
